dee28e5787a62edc149279e52f08ba27dad55317
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2023-02-08 20:43:49 -0500

    Add extra space

--- adafruit_testrepo.py
+++ adafruit_testrepo.py
@@ -45,7 +45,7 @@ class Test:
         while True:
             pass
 
-    def test_method(self):
+    def test_method(self): 
         """Test method."""
         return "Test value {}".format(self._test_value)
 
